8978dadbddc2811ffbc767193ddbaff20cc43086
c4ts0up <a.baccap@uniandes.edu.co>
2024-03-22 16:16:51 -0500

    Created class to standarize crashlytic reporting. Project errors shown by Android Studio, yet it is compilable.

---
+++ app/src/main/java/com/example/ventura/model/analytics/FeatureCrashHandler.kt
@@ -0,0 +1,20 @@
+package com.example.ventura.model.analytics
+
+import android.util.Log
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.crashlytics.setCustomKeys
+
+class FeatureCrashHandler(screenName: String) {
+    private val screenName = screenName
+
+    public fun logCrash(featureName: String, e: Exception) {
+        Log.d("crashlytics", "Crash logged")
+        FirebaseCrashlytics.getInstance().setCustomKeys {
+            key("screen", screenName)
+            key("feature", featureName)
+        }
+        FirebaseCrashlytics.getInstance().log("Test Crash Class")
+        FirebaseCrashlytics.getInstance().recordException(e)
+        FirebaseCrashlytics.getInstance().sendUnsentReports()
+    }
+}
