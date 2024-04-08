7f705d464bf20ae5b9955bdaa38deafd46356bfe
c4ts0up <a.baccap@uniandes.edu.co>
2024-04-08 10:38:15 -0500

    Nonexistent WeatherResponse class created to avoid null manipulation.

--- app/src/main/java/com/example/ventura/model/WeatherResponse.kt
+++ app/src/main/java/com/example/ventura/model/WeatherResponse.kt
@@ -1,15 +1,31 @@
 package com.example.ventura.model
 
-data class WeatherResponse(
-    val weather: List<Weather>,
-    val main: Main
-)
+sealed class WeatherResponse(val weather : List<Weather>, val main: Main) {
+
+    // nonexistent class to avoid null manipulation
+    object Nonexistent : WeatherResponse(emptyList(), Main.Nonexistent) {
+        override fun toString(): String {
+            return "Weather data unavailable"
+        }
+    }
+
+    data class Data(val actualData: WeatherResponse) : WeatherResponse(actualData.weather, actualData.main)
+}
+
 
 data class Weather(
     val description: String
 )
 
-data class Main(
-    val temp: Double,
-    val humidity: Double
-)
+// FIX: ambiguous name
+sealed class Main(val temp: Double, val humidity: Double) {
+
+    // nonexistent class to avoid null manipulation
+    object Nonexistent : Main(0.0, 0.0) {
+        override fun toString(): String {
+            return "Main data unavailable"
+        }
+    }
+
+    data class Data(val actualData: Main) : Main(actualData.temp, actualData.humidity)
+}
